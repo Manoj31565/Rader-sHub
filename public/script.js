eec7e84cd1333f43311d39c8205a01ef7707f218
Manoj31565 <105906837+Manoj31565@users.noreply.github.com>
2025-03-28 17:16:31 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,20 @@
+﻿document.getElementById("homeButtonMain").addEventListener("click", function ()
+{
+    alert("Home page opened...");
+});
+document.getElementById("productsButton").addEventListener("click", function ()
+{
+    alert("Products page opened...");
+});
+document.getElementById("servicesButton").addEventListener("click", function ()
+{
+    alert("Services page opened...");
+});
+document.getElementById("forumsButton").addEventListener("click", function ()
+{
+    alert("Forums page opened...");
+});
+document.getElementById("registerButton").addEventListener("click", function ()
+{
+    alert("Register page opened...");
+});
